017ef7d0b8514924f54dfe12a99669ed5af56244
SuperRookieMam <422375723@qq.com>
2018-11-16 17:29:29 +0800

    2018年10月25日16:51:06

--- yhlcore/yhl-parent/yhl-base/src/main/java/com/yhl/base/dao/BaseDao.java
+++ yhlcore/yhl-parent/yhl-base/src/main/java/com/yhl/base/dao/BaseDao.java
@@ -1,9 +1,24 @@
 package com.yhl.base.dao;
 
+import com.yhl.base.dto.Params;
+
+import java.util.List;
+
 public interface BaseDao {
 
     public  <T> T findById(String Id);
 
-    public <T>  T findByWhere();
+    public <T> List<T> findByWhere(Params params);
 
+    public <T> int  SelectCountByWhere(Params params);
+
+    public <T> int save(T object);
+
+    public <T> int update(T object);
+
+    public <T> int deleteByWhere(Params params);
+
+    public <T> List<T> findBysql(String sql,Params params);
+
+    public <T> int findByHql(String sql,Params params);
 }
