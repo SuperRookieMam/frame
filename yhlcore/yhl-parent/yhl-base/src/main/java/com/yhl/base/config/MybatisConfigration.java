63f7e38c02f23f195a1ffd72f49dd7231cda492d
SuperRookieMam <422375723@qq.com>
2018-11-29 18:02:31 +0800

    2018年10月25日16:51:06

--- yhlcore/yhl-parent/yhl-base/src/main/java/com/yhl/base/config/MybatisConfigration.java
+++ yhlcore/yhl-parent/yhl-base/src/main/java/com/yhl/base/config/MybatisConfigration.java
@@ -4,17 +4,23 @@ package com.yhl.base.config;
 import com.yhl.base.condition.MybatisCondtion;
 import com.yhl.base.condition.PrimaryCondition;
 import com.yhl.base.condition.SecondCondition;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
 @Conditional(MybatisCondtion.class)
-@MapperScan(basePackages = {"com.**.dao","com.**.mapper"})
+@MapperScan(basePackages = {"com.**.dao.mybatis"})
 public class MybatisConfigration {
 
 
@@ -33,4 +39,44 @@ public class MybatisConfigration {
     public DataSource getSecondDataSource(){
         return DataSourceBuilder.create().build();
     }
+//=======================================================================================
+    /*
+    * mybatis primary
+    * */
+    @Primary
+    @Conditional(PrimaryCondition.class)
+    @Bean(name = "primarySqlSessionFactory")
+    public SqlSessionFactory getPrimarySqlSessionFactory(@Qualifier("primayDatasource") DataSource dataSource) throws  Exception{
+        if (dataSource!=null){
+            SqlSessionFactoryBean sqlSessionFactoryBean =new SqlSessionFactoryBean();
+            sqlSessionFactoryBean.setDataSource(dataSource);
+            PathMatchingResourcePatternResolver solver =new PathMatchingResourcePatternResolver();
+            Resource[] resources = solver.getResources("classpath*:mapper/**/*.xml");
+            sqlSessionFactoryBean.setMapperLocations(resources);
+            return sqlSessionFactoryBean.getObject();
+          }
+        return null;
+    }
+    @Primary
+    @Conditional(PrimaryCondition.class)
+    @Bean(name = "primarySqlSessionTemplate")
+    public SqlSessionTemplate getPrimarySqlSessionTemplate(@Qualifier("primarySqlSessionFactory") SqlSessionFactory sqlSessionFactory){
+         if (sqlSessionFactory!=null){
+             return new SqlSessionTemplate(sqlSessionFactory);
+         }
+        return null;
+    }
+
+//=======================================================================================
+
+
+
+
+
+
+
+
+
+
+
 }
