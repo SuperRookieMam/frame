bb73e6833e54b274f403f81794bdbbeb16667d3c
SuperRookieMam <422375723@qq.com>
2018-11-30 18:01:46 +0800

    zhas

---
+++ yhlcore/yhl-parent/mybatis/src/main/java/demo/dao/BaseDao.java
@@ -0,0 +1,21 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+public interface BaseDao<T> {
+
+    /**
+     * 根据id查询实体
+     * @param id
+     * @return  T 实体
+     * */
+    public <T> T findById(String id);
+
+    /**
+     * 根据条件查询实体
+     * */
+    public <T> List<T> findByParams();
+
+
+
+}
