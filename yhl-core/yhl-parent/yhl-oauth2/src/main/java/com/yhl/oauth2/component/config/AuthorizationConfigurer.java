d81dd9296216e679c2ada1992f8b37b0a13bdae5
绝世好男人 <422375723@qq.com>
2019-01-24 00:34:48 +0800

    sj

--- yhl-core/yhl-parent/yhl-oauth2/src/main/java/com/yhl/oauth2/component/config/AuthorizationConfigurer.java
+++ yhl-core/yhl-parent/yhl-oauth2/src/main/java/com/yhl/oauth2/component/config/AuthorizationConfigurer.java
@@ -1,13 +1,21 @@
 package com.yhl.oauth2.component.config;
 
 
+import com.yhl.oauth2.service.MyAuthorizationServerTokenServices;
 import com.yhl.oauth2.service.MyClientDetailService;
+import com.yhl.oauth2.service.UserApprovalStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.approval.ApprovalStoreUserApprovalHandler;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
 
 /**
  * auoth  用来配置授权（authorization）以及令牌（token）
@@ -20,37 +28,40 @@ public class AuthorizationConfigurer extends AuthorizationServerConfigurerAdapte
     @Autowired
     private MyClientDetailService myClientDetailService;
 
-
+    @Autowired
+    private UserApprovalStoreService userApprovalStoreService;
+    @Autowired
+    private MyAuthorizationServerTokenServices myAuthorizationServerTokenServices;
        /**
-         *
-         * ClientDetailsService，有几个重要的属性如下列表：
-         *clientId：（必须的）用来标识客户的Id。
-         *secret：（需要值得信任的客户端）客户端安全码，如果有的话。
-         *scope：用来限制客户端的访问范围，如果为空（默认）的话，那么客户端拥有全部的访问范围。
-         *authorizedGrantTypes：此客户端可以使用的授权类型，默认为空。
-         *authorities：此客户端可以使用的权限（基于Spring Security authorities）。
-        *
         * ClientDetailsServiceConfigurer：用来配置客户端详情服务（ClientDetailsService），
-        * 客户端详情信息在这里进行初始化，你能够把客户端详情信息写死在这里或者是通过数据库来存储调取详情信息
-         */
-    @Override  //此方法主要是用来配置token的存储方式
+        * 获取客户端的详细信息，可以继承ClientDetailsService 来自定义管理
+        * 注意client实体对应的属性
+        *
+        */
+    @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        //super.configure(clients);
-        /*clients.inMemory()                //使用in-memory存储：存在内存里面
-                    .withClient("normal-app")   //client_id 客户端ID 对应一个客户端
-                    .authorizedGrantTypes("authorization_code", "implicit") // 该client允许的授权类型
-                    .authorities("ROLE_CLIENT")
-                    .scopes("read","write")   // 允许的授权范围,读写
-                    .resourceIds(resourceId)
-                    .accessTokenValiditySeconds(accessTokenValiditySeconds)//授权码存活时间;
-                    .and()
-                    */
           // 通过自定义的service来存储客户端信息
           clients.withClientDetails(myClientDetailService);
     }
     /**
-     * AuthorizationServerEndpointsConfigurer:用来配置授权（authorization）以及令牌（token）
-     * 的访问端点和令牌服务(token services)
+     * AuthorizationServerSecurityConfigurer ：
+     * 用来配置令牌端点(Token Endpoint)的安全约束.
+     * */
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        super.configure(security);
+        security
+                // 开启/oauth/token_key验证端口无权限访问
+                .tokenKeyAccess("permitAll()")
+                // 开启/oauth/check_token验证端口认证权限访问
+                .checkTokenAccess("isAuthenticated()");
+
+    }
+
+    /**
+     * AuthorizationServerEndpointsConfigurer:
+     * 用来配置授权（authorization）以及令牌（token） 的访问端点
+     * 和令牌服务(token services)
      *
      * 配置授权类型（Grant Types）：
      * 授权是使用 AuthorizationEndpoint 这个端点来进行控制的，
@@ -72,16 +83,31 @@ public class AuthorizationConfigurer extends AuthorizationServerConfigurerAdapte
      * implicitGrantService：这个属性用于设置隐式授权模式，用来管理隐式授权模式的状态。
      *
      * tokenGranter：这个属性就很牛B了，当你设置了这个东西（即 TokenGranter 接口实现），
-     * 那么授权将会交由你来完全掌控，并且会忽略掉上面的这几个属性，这个属性一般是用作拓展用途的，
+     * 那么授权将会交由你来完全掌控，并且会忽略掉上面的这几个属性，
+     * 这个属性一般是用作拓展用途的，
      * 即标准的四种授权模式已经满足不了你的需求的时候，才会考虑使用这个。
      * */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-
-
+        //自定义了对AccessToken的相关操作创建、刷新、获取
+        endpoints.tokenServices(myAuthorizationServerTokenServices);
+        // 用户授权处理逻辑
+        endpoints.userApprovalHandler(userApprovalHandler());
+        //当不上面属性不能满足自己时可以自己实现tokenGranter来自定义 权限逻辑
+        //endpoints.tokenGranter();
+    }
+    @Bean
+    public UserApprovalHandler userApprovalHandler(){
+        ApprovalStoreUserApprovalHandler userApprovalHandler =new  ApprovalStoreUserApprovalHandler();
+        //自定义的权限存储方式
+        userApprovalHandler.setApprovalStore(userApprovalStoreService);
+        //客户端请求信息的
+        userApprovalHandler.setRequestFactory(requestFactory());
+        return  userApprovalHandler;
     }
 
-
-
-
+    @Bean
+    public OAuth2RequestFactory requestFactory() {
+        return new DefaultOAuth2RequestFactory(myClientDetailService);
+    }
 }
---
+++ yhl-core/yhl-parent/yhl-oauth2/src/main/java/com/yhl/oauth2/service/MyAuthorizationServerTokenServices.java
@@ -0,0 +1,6 @@
+package com.yhl.oauth2.service;
+
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+
+public interface MyAuthorizationServerTokenServices extends AuthorizationServerTokenServices {
+}
