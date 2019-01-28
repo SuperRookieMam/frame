69da95cb8f8cea08a7c5e624b7a2877b81e6af93
绝世好男人 <422375723@qq.com>
2019-01-29 00:06:30 +0800

    sj

--- yhl-core/yhl-parent/yhl-oauth2/src/main/java/com/yhl/oauth2/component/config/OAuthServerConfigurer.java
+++ yhl-core/yhl-parent/yhl-oauth2/src/main/java/com/yhl/oauth2/component/config/OAuthServerConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
@@ -23,5 +24,23 @@ public class OAuthServerConfigurer   extends AuthorizationServerConfigurerAdapte
         // 通过自定义的service来存储客户端信息
         clients.withClientDetails(myClientDetailService);
     }
+  /**
+   * AuthorizationServerSecurityConfigurer ：
+   * 用来配置令牌端点(Token Endpoint)的安全约束.
+   * */
+  @Override
+  public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+    super.configure(security);
+    // 通常情况下,Spring Security获取token的认证模式是基于http basic的,
+    // 也就是client的client_id和client_secret是通过http的header或者url模式传递的，
+    // 也就是通过http请求头的 Authorization传递，具体的请参考http basic
+    // 或者http://client_id:client_secret@server/oauth/token的模式传递的
+    // 当启用这个配置之后，server可以从表单参数中获取相应的client_id和client_secret信息
+    // 默认情况下，checkToken的验证时denyAll的，需要手动开启
+    security.checkTokenAccess("isAuthenticated()"); // 开启/oauth/check_token验证端口认证权限访问
+    security.allowFormAuthenticationForClients();//允许对客户机进行表单身份验证
+    //security.tokenKeyAccess("permitAll()");// 开启/oauth/token_key验证端口无权限访问
+  }
+
 
 }
